056008b3232bde64b79d547429f94b9999615a00
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-06-10 11:45:38 +0600

    project put api crated

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express()
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
   "mongodb+srv://nextfolio:nextfolio@cluster0.zhsy6ko.mongodb.net/?retryWrites=true&w=majority";
 
@@ -44,22 +45,88 @@ async function run() {
         });
       }
     });
+
+    app.get("/api/v1/project/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        console.log(id);
+        const query = { _id: new ObjectId(id) };
+        const singleProject = await project.findOne(query);
+        // console.log(singleProject);
+        if (!singleProject) {
+          return res.send(404).json({
+            success: false,
+            message: "Project not found",
+          });
+        }
+        res.status(200).json({ 
+          success: true,
+          message: 'Project retrieved successfully',
+          data: singleProject
+      });
+      } catch (error) {
+        console.error("Error fetching good:", error);
+        res.status(500).json({
+          success: false,
+          message: "Internal server error",
+        });
+      }
+    });
     app.post("/api/v1/project", async (req, res) => {
       try {
         const { name, img1, img2, img3, img4, techs, live, github } = req.body;
-        console.log(name, img1, img2, img3, img4, techs, live, github);
-        const newProject={
-          name, img1, img2, img3, img4, techs, live, github 
-        }
-        console.log(newProject)
-        const result=await project.insertOne(newProject)
-            res.status(200).json({
-                success: true,
-                message: "New Project found successfully",
-                data: newProject,
-              });
+        // console.log(name, img1, img2, img3, img4, techs, live, github);
+        const newProject = {
+          name,
+          img1,
+          img2,
+          img3,
+          img4,
+          techs,
+          live,
+          github,
+        };
+        // console.log(newProject)
+        const result = await project.insertOne(newProject);
+        res.status(200).json({
+          success: true,
+          message: "New Project found successfully",
+          data: newProject,
+        });
       } catch (error) {
         console.error("Error fetching users:", error);
+        res.status(500).json({
+          success: false,
+          message: "Internal server error",
+        });
+      }
+    });
+
+    app.put("/api/v1/projects/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        console.log(id);
+        const { name, img1, img2, img3, img4, techs, live, github } = req.body;
+        const filter = { _id: new ObjectId(id) };
+        const updateProject = {
+          $set: {
+            name,
+            img1,
+            img2,
+            img3,
+            img4,
+            techs,
+            live,
+            github,
+          },
+        };
+        const result = await project.updateOne(filter, updateProject);
+        res.status(200).json({
+          success: true,
+          message: "Projec Updated successfully",
+          data: result,
+        });
+      } catch (error) {
         res.status(500).json({
           success: false,
           message: "Internal server error",
