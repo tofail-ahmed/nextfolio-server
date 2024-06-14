11e33c95ffccd6c8b21e8cfab9e41605a7809131
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-06-15 00:20:09 +0600

    setter and getter added

--- set_get.js
+++ set_get.js
@@ -11,8 +11,23 @@ const person = {
 	}, 
 }; 
 
-console.log(person.fullName); // "John Doe" 
+// console.log(person.fullName); // "John Doe" 
 
-person.fullName = "Jane Smith"; 
-console.log(person.firstName); // "Jane" 
-console.log(person.lastName); // "Smith"
+// person.fullName = "Jane Smith"; 
+// console.log(person.firstName); // "Jane" 
+// console.log(person.lastName); // "Smith"
+
+const temperature = { 
+	_celsius: 0, 
+	get fahrenheit() { 
+		return this._celsius * 1.8 + 32; 
+	}, 
+	set fahrenheit(value) { 
+		this._celsius = (value - 32) / 1.8; 
+	}, 
+}; 
+
+console.log(temperature.fahrenheit); // 32 
+temperature.fahrenheit = 78; 
+console.log(temperature._celsius); // 20
+
